3c77aee2e6dfa6ccd239822c055e91858f99beda
Diego Lechuga <diegolechuga2003@gmail.comgit config --list>
2024-10-21 20:04:30 -0600

    feat: ciclos

---
+++ 2.1.1-Ciclos/main.js
@@ -0,0 +1,69 @@
+for(let i = 0; i < 5; i++){
+    console.log(i);
+}
+
+console.log("El ciclo terminó");
+
+
+let limite = 100;
+
+for(let i = 0; i <= limite; i++){
+    if(i % 2 === 0){
+        console.log(`El número ${i} es par`);
+    } else {
+        console.log(`El número ${i} es impar`);
+    }
+}
+
+let mensaje = "Hola mundo";
+let mensajeInvertido = "";
+
+for(let i = mensaje.length - 1; i >= 0; i--){
+    mensajeInvertido += mensaje.charAt(i);
+}
+
+console.log(mensaje);
+console.log(mensajeInvertido);
+
+let index = 0;
+
+while(index <= 10){
+    console.log(index);
+    index++;
+}
+
+let numeroAleatorio = Math.floor(Math.random()*100);
+
+while(numeroAleatorio !== 50){
+    console.log(numeroAleatorio);
+    numeroAleatorio = Math.floor(Math.random()*100);
+}
+
+let numeroAleatorio2 = 50;
+
+do{
+    console.log(numeroAleatorio2);
+    numeroAleatorio2 = Math.floor(Math.random()*3);
+}while(numeroAleatorio2 !== 2)
+
+console.log(numeroAleatorio2);
+
+let numeroAleatorio3 = Math.floor(Math.random()*100);
+
+for(let i = 0; i < 2; i++){
+    console.log(numeroAleatorio3);
+    numeroAleatorio3 = Math.floor(Math.random()*100);
+}
+
+
+
+let pin = 2424;
+
+let numero = prompt("Escribeme un número");
+
+console.log(numero);
+
+let numero2 = prompt("Dame otro número");
+
+console.log(numero2);
+
